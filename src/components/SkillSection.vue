c3af39049508cccc516a7fdc0fa6c5765faa8712
Gareth618 <bloggareth@gmail.com>
2023-02-11 14:35:20 +0200

    started second section

---
+++ src/components/SkillSection.vue
@@ -0,0 +1,31 @@
+<script>
+import CurveDivider from './CurveDivider.vue';
+
+export default {
+  components: {
+    CurveDivider
+  }
+};
+</script>
+
+<template>
+  <section>
+    <CurveDivider />
+    <h1>Skilluri & Cunoștințe</h1>
+  </section>
+</template>
+
+<style scoped>
+section {
+	position: relative;
+  padding: calc(min(10vh, 15vw) + 2rem) 3rem 2rem 3rem;
+	background-image: linear-gradient(45deg, dodgerblue, royalblue);
+}
+
+h1 {
+  font-size: 5rem;
+  color: white;
+  text-underline-offset: .1em;
+  text-decoration: underline solid #ccc .1em;
+}
+</style>
