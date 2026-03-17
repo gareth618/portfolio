0561e90987547ada5616f031e0dfac5165b2461c
Iulian Oleniuc <iulian.oleniuc@gmail.com>
2026-03-17 23:59:10 +0200

    Set up Vitest and tested utilities
    I have also refactored the `slugify` function to handle any kind of
    accent symbols and the `dateToString` function to only handle proper
    dates (no year-only numbers).

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,10 +1,6 @@
-export function dateToString(date: number | Date) {
-  if (!(date instanceof Date)) return date.toString()
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const yy = year.toString()
-  const mm = (month < 10 ? '0' : '') + month.toString()
-  const dd = (day < 10 ? '0' : '') + day.toString()
-  return `${dd} / ${mm} / ${yy}`
+export function dateToString(date: Date) {
+  const dd = String(date.getDate()).padStart(2, '0')
+  const mm = String(date.getMonth() + 1).padStart(2, '0')
+  const yyyy = date.getFullYear()
+  return `${dd} / ${mm} / ${yyyy}`
 }
