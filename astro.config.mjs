6959617cf11bc1f81a9fbe7919b981a4a605a0e1
Iulian Oleniuc <iulian.oleniuc@gmail.com>
2026-03-17 23:21:31 +0200

    Added KaTeX support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
+import rehypeKatex from 'rehype-katex'
+import remarkMath from 'remark-math'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@astrojs/vue'
 
@@ -12,7 +14,8 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   markdown: {
-    remarkPlugins: [linkPlugin, textPlugin],
+    remarkPlugins: [remarkMath, linkPlugin, textPlugin],
+    rehypePlugins: [rehypeKatex],
     shikiConfig: {
       theme: 'one-dark-pro',
     },
